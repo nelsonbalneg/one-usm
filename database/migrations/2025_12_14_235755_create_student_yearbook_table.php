73c2f37656b26940989c25835067abe41a84d1fe
nelsonbalneg <nelsonbalneg@gmail.com>
2026-04-03 15:46:26 +0800

    Update april 4

--- database/migrations/2025_12_14_235755_create_student_yearbook_table.php
+++ database/migrations/2025_12_14_235755_create_student_yearbook_table.php
@@ -8,26 +8,36 @@ return new class extends Migration {
     /**
      * Run the migrations.
      */
+
+
+
     public function up(): void
     {
-        Schema::create('student_yearbook', function (Blueprint $table) {
-            $table->id();
-            $table->string('student_id'); // same type as portal_users.student_id
-            $table->text('motto')->nullable();
-            $table->json('awards')->nullable();
-            $table->json('hobbies')->nullable();
-            $table->json('organizations')->nullable();
-            $table->json('trainings')->nullable();
-            $table->text('ojt_experience')->nullable();
-            $table->text('memorable_experience')->nullable();
-            $table->text('career_goal')->nullable();
-            $table->text('favorite_quote')->nullable();
-            $table->string('facebook')->nullable();
-            $table->string('linkedin')->nullable();
-            $table->timestamps();
-
-            $table->foreign('student_id')->references('student_id')->on('portal_users')->onDelete('cascade');
+        Schema::table('portal_users', function (Blueprint $table) {
+            $table->unique('student_id');
         });
+
+        Schema::create('dbo.student_yearbook', function (Blueprint $table) {
+        $table->id();
+        $table->string('student_id'); // same type as portal_users.student_id
+        $table->text('motto')->nullable();
+        $table->json('awards')->nullable();
+        $table->json('hobbies')->nullable();
+        $table->json('organizations')->nullable();
+        $table->json('trainings')->nullable();
+        $table->text('ojt_experience')->nullable();
+        $table->text('memorable_experience')->nullable();
+        $table->text('career_goal')->nullable();
+        $table->text('favorite_quote')->nullable();
+        $table->string('facebook')->nullable();
+        $table->string('linkedin')->nullable();
+        $table->timestamps();
+
+        $table->foreign('student_id')
+            ->references('student_id')
+            ->on('dbo.portal_users') // 👈 include schema here too
+            ->onDelete('cascade');
+    });
 
     }
 
